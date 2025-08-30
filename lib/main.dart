93f945bbd31b74a262df81a634888ffe2397bc48
mihiranwijesekara <achinthamihiran654@gmail.com>
2025-08-30 17:32:04 +0530

    create ar navigator

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,28 @@
+import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
+import 'package:login_signup_ui/ar_navigator_screen.dart';
 import 'package:login_signup_ui/forgot_password_screen';
 import 'package:login_signup_ui/home_screen.dart';
 import 'package:login_signup_ui/login_screen.dart';
 import 'package:login_signup_ui/signup_screen.dart';
 
 void main() async {
-  runApp(const MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+
+  List<CameraDescription> cameras = [];
+  try {
+    cameras = await availableCameras();
+  } catch (e) {
+    print('Error initializing cameras: $e');
+  }
+
+  runApp(MyApp(cameras: cameras));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({super.key, required this.cameras});
+
+  final List<CameraDescription> cameras;
 
   @override
   Widget build(BuildContext context) {
@@ -23,6 +36,7 @@ class MyApp extends StatelessWidget {
         '/signup': (_) => const SignUpScreen(),
         '/forgot-password': (_) => const ForgotPasswordScreen(),
         '/home': (_) => const HomeScreen(),
+        '/ARNavigator': (_) => NavigationScreen(cameras: cameras),
       },
     );
   }
